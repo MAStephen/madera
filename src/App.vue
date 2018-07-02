0398f5818ac5bfba995a84f1a9d905175963d9ca
MAStE-kamomille <stenm64@gmail.com>
2018-07-02 16:20:06 +0200

    creation du main, supp acceuil.vue
    Suppression acceuil.vue
    Implementation App.vue
    Rajout ligne index.html
    + legere modif

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div>
+      <div class="navbar-fixed">
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo left">{{titre}}</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><i class="small material-icons">insert_chart</i></li>
+              <li><a href="#">{{menu_1}}</a></li>
+              <li><i class="small material-icons">dashboard</i></li>
+              <li><a href="#">{{menu_2}}</a></li>
+              <li><i class="small material-icons">date_range</i></li>
+              <li><a href="#">{{menu_3}}</a></li>
+              <li><i class="small material-icons">contacts</i></li>
+              <li><a href="#">{{menu_4}}</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+  <router-view/>
+
+      <footer class="page-footer blue-grey">
+        <div class="footer-copyright">
+          <div class="container"> <p>© 2018 Copyright 
+          <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+          </p>
+          </div>
+        </div>
+      </footer>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      titre: 'MADERA',
+      menu_1: 'Les démarches',
+      menu_2: 'Nos idées',
+      menu_3: 'RDV',
+      menu_4: 'Nous'
+    } 
+  }
+     
 }
 </script>
 
@@ -17,6 +56,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+body {
+  margin: auto;
+  padding: 0px;
+  width: 1550px;
+}
+
+nav{
+  background-color: #607d8b;
+  margin: auto;
+  width: 1550px;
+  padding: 0px;
+}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-/*import HelloWorld from '@/components/HelloWorld'*/
-import acceuil from '@/components/acceuil'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -9,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'acceuil',
-      component: acceuil
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })

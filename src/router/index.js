4706b29ceaab21e8f2280aee53b33efe1f3bdfc2
MAStE-kamomille <stenm64@gmail.com>
2018-07-02 14:42:56 +0200

    new vue + modif package
    Package a jour
    Création de la vue accueil

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+/*import HelloWorld from '@/components/HelloWorld'*/
+import acceuil from '@/components/acceuil'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'acceuil',
+      component: acceuil
     }
   ]
 })
